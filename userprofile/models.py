7d35bab6a8310f711631029860a97e8810665c74
aioont <aioont8@gmail.com>
2023-02-27 08:38:17 +0530

    Added files 27-2 in morning payment function broken

---
+++ userprofile/models.py
@@ -0,0 +1,80 @@
+from random import choices
+from django.db import models
+# from store.models import Vendor
+# from .models import BecomeVendor
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+
+class CustomUserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    def _create_user(self, email, password=None, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    mobile = models.CharField(max_length=15, null=True, blank= True)
+    is_verified = models.BooleanField(default=False)
+    forget_password = models.CharField(max_length=255, blank= True, null=True)
+    is_vendor = models.BooleanField(default=False)
+  
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['mobile']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.first_name
+
+class BecomeVendor(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)
+    vendor_name = models.CharField(max_length=255)
+    vendor_email = models.EmailField(max_length=100, unique=True)
+    vendor_address = models.CharField(max_length=255)
+    vendor_image = models.ImageField(upload_to='uploads/vendor_images/', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.user:
+            raise ValueError('A CustomUser instance is required for saving a BecomeVendor instance')
+        super(BecomeVendor, self).save(*args, **kwargs)
+        self.user.is_vendor = True
+        self.user.save()
+
+    class Meta:
+        verbose_name_plural = "Become a service provider"
+
+    def __str__(self):
+        return self.vendor_name
+
